7357b9ef2d723e31ab09b33fd7682f23deb4650c
Mayaisafa <115871861+Mayaisafa@users.noreply.github.com>
2022-10-26 15:23:52 +0700

    Add files via upload

---
+++ P1_Data Definition Language & Data Manipulation Language.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Isafa (
+NIM VARCHAR PRIMARY KEY,
+NamaDepan VARCHAR (10),
+NamaBelakang VARCHAR (10),
+NomerHP VARCHAR (14)
+)
+
+-- Cara Input Data
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+VALUES (2100015041,"sindy","Marantika",085840565100)
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+Values (2200015023,"maya","saputri","085643771945")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2200015032,"anggi","aryanawa","08989220177")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2200015012,"didi","cahyadi",083824034448)
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015050,"lely","saputri","085640961542")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015051,"saiful","andika","082241617784")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015036,"anggit","larasati","085846933321")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015002,"ragita","anillya","081292147880")
+ 
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015010,"natasya","dewi","085710700182")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015037,"lisa","safitri","082253059683")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015049,"wisnu","catur","081232197053")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2100015006,"januar","rasyid","081229088740")
+
+INSERT INTO Isafa (nim,NamaDepan,NamaBelakang,NomerHP)
+values (2215015035,"hasan","darmawan","085694294438")
+
+select * from Isafa
